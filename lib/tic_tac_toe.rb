819609628ca87b4b73bdea43801af77842285332
Leon Malisov <leonmalisov@gmail.com>
2019-12-02 17:55:10 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,6 +17,22 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8] }"
 end
 
+def valid_move?(board, index)
+  if position_taken?(board, index) == false && index <= 8
+    true
+  else
+    false
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
+
 def won?(board)
   winner = false
   winning_combination = false
